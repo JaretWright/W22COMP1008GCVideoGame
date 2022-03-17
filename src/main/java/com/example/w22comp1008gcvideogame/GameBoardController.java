87c1a7ab31c9539c77b91d9ddc4687d4b66b960b
JaretWright <jaret.wright@georgiancollege.ca>
2022-03-17 12:21:31 -0400

    Added the setOnKeyReleased and setOnKeyPressed methods

--- src/main/java/com/example/w22comp1008gcvideogame/GameBoardController.java
+++ src/main/java/com/example/w22comp1008gcvideogame/GameBoardController.java
@@ -2,12 +2,17 @@ package com.example.w22comp1008gcvideogame;
 
 import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+
+import java.util.HashSet;
 
 public class GameBoardController {
 
@@ -17,10 +22,29 @@ public class GameBoardController {
     @FXML
     private Button startButton;
 
+    private HashSet<KeyCode> activeKeys;
+
     @FXML
     private void startGame(ActionEvent event)
     {
         startButton.setVisible(false);
+        activeKeys = new HashSet<>();
+
+        //This is an example of an anonymous inner class
+        anchorPane.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyPressed) {
+                System.out.println(keyPressed.getCode() + "-> activeKeys: "+ activeKeys);
+                activeKeys.add(keyPressed.getCode());
+            }
+        });
+
+        anchorPane.getScene().setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyReleased) {
+                activeKeys.remove(keyReleased.getCode());
+            }
+        });
 
         //a canvas can be used to "draw" on.  The GraphicsContext is the tool used for the drawing
         Canvas canvas = new Canvas(GameConfig.getGame_width(), GameConfig.getGame_height());
